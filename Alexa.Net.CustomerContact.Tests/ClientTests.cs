51d3398c813dc0ccfeb66cdbf72493c7c1f764d0
Steven Pears <github@lotsofideas.co.uk>
2018-08-02 14:16:56 +0200

    Client creation working

--- Alexa.Net.CustomerContact.Tests/ClientTests.cs
+++ Alexa.Net.CustomerContact.Tests/ClientTests.cs
@@ -1,14 +1,41 @@
 using System;
+using System.Net.Http;
+using Alexa.NET.Request;
 using Xunit;
 
 namespace Alexa.Net.CustomerContact.Tests
 {
-    public class UnitTest1
+    public class ClientTests
     {
         [Fact]
-        public void Test1()
+        public void CreateFromSkillRequestSetupCorrectly()
         {
+			const string endpoint = "https://testclient/";
+			const string accesstoken = "accesstoken";
 
+			var request = new SkillRequest
+			{
+				Context = new Context{
+					System = new AlexaSystem{
+						ApiEndpoint = endpoint,
+						ApiAccessToken = accesstoken
+					}
+				}
+			};
+
+			var client = new CustomerContactClient(request);
+
+			Assert.Equal(endpoint, client.Client.BaseAddress.ToString());
+			Assert.Equal(accesstoken, client.Client.DefaultRequestHeaders.Authorization.Parameter);
         }
+
+        [Fact]
+		public void ClientSetDirectlySetCorrectly(){
+			var lowClient = new HttpClient();
+
+			var client = new CustomerContactClient(lowClient);
+
+			Assert.Equal(lowClient, client.Client);
+		}
     }
 }
--- Alexa.Net.CustomerContact/CustomerContactClient.cs
+++ Alexa.Net.CustomerContact/CustomerContactClient.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Alexa.NET.Request;
+
 namespace Alexa.Net.CustomerContact
 {
     public class CustomerContactClient
     {
-        public CustomerContactClient(SkillRequest request)
+		public HttpClient Client { get; set; }
+		public CustomerContactClient(SkillRequest request):this(
+			request.Context.System.ApiEndpoint,
+			request.Context.System.ApiAccessToken){}
+
+		public CustomerContactClient(string endpointUrl, string accessToken)
         {
+            var client = new HttpClient { BaseAddress = new Uri(endpointUrl, UriKind.Absolute) };
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            Client = client;
+        }
+
+		public CustomerContactClient(HttpClient client)
+        {
+            Client = client;
         }
     }
 }
